f4f8680206b3fea826d3ab5b1e0ce51eaf39978c
Konrad Ciepluch <konrad190008@wp.pl>
2021-05-26 18:03:41 +0200

    Updates

---
+++ precache-manifest.086aae5d404e4fe9fc538643ca3ff059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13cbce451a21fdce3e35",
+    "url": "/react-router-samuraj/static/css/main.eab38ebb.chunk.css"
+  },
+  {
+    "revision": "13cbce451a21fdce3e35",
+    "url": "/react-router-samuraj/static/js/main.34269adc.chunk.js"
+  },
+  {
+    "revision": "a96e56866b828d06a1b3",
+    "url": "/react-router-samuraj/static/js/runtime~main.e2acb02d.js"
+  },
+  {
+    "revision": "184f237f5a55dc963fa4",
+    "url": "/react-router-samuraj/static/js/2.9d6172d8.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-router-samuraj/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-router-samuraj/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-router-samuraj/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "18cb08673c87123e780721ddc576e8d3",
+    "url": "/react-router-samuraj/index.html"
+  }
+];
